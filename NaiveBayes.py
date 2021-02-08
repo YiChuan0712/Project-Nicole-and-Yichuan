8d09ec08362e6899042ea36889ad1abf77310ea5
YiChuan0712 <52645610+YiChuan0712@users.noreply.github.com>
2021-02-09 00:09:20 +0800

    Update NaiveBayes.py

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -3,7 +3,7 @@ import numpy as np
 import random
 
 data = pd.read_csv(r"D:\Datasets\village_130_1.csv")#, index_col=0)
-data['Outcome'] = data['CF (Outcome Numeric)']
+data['Outcome'] = data['CF (Outcome Numeric)'] #把outcome移动到最后一列
 data.drop(['CF (Outcome Numeric)'], axis=1, inplace=True)
 
 data.drop(['Row'], axis=1, inplace=True)
@@ -23,13 +23,15 @@ data.drop(['Duration (sec)'], axis=1, inplace=True)
 data.info()
 # print(data)
 
-l = list(data.index)
-random.shuffle(l)
-data.index = l
-n = data.shape[0]
-m = int(n * 0.8)
-train = data.loc[range(m), :]
-test = data.loc[range(m, n), :]
+l = list(data.index) # 得到索引
+random.shuffle(l) # 随机排序
+data.index = l # 随机排序后的索引赋给数据集
+n = data.shape[0] # 行数
+m = int(n * 0.8) # 训练集数量
+train = data.loc[range(m), :] # 前m个训练集
+test = data.loc[range(m, n), :] #测试集
+
+# 更新一下索引
 data.index = range(data.shape[0])
 test.index = range(test.shape[0])
 
